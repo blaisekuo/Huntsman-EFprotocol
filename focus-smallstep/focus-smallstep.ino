7e79bf7b06a45ab46b94e3953bc334f1e17577e0
Blaise C. Kuotiong <kuotiong@cfht.hawaii.edu>
2018-08-27 14:05:14 +1000

    added french reverse engineering documentation

--- focus-smallstep/focus-smallstep.ino
+++ focus-smallstep/focus-smallstep.ino
@@ -10,16 +10,21 @@ int RetVal;
 
 int x,y,offset;
 
-int stepsize = 500;
+int stepsize = 50;
  
 void setup() // initialization 
 {     
 
 delay(300);
-  CLKPR = B10000000;
-  CLKPR = B00000001;
+//arduino clock is 16 mhz
+//following lines half the clock speed to 8 Mhz
+//SPI_CLOCK further down, divides it more to the 62 to 500 khz range needed
+//for communicating with cannon lenses
+//for 500 khz, comment out the CLKPR lines and change to SPI_CLOCK_DIV32
+//  CLKPR = B10000000;
+//  CLKPR = B00000001;
   
-  Serial.begin(38400);  
+  Serial.begin(115200);  
   Serial.println("--- Start Serial Monitor SEND_RCVE ---");
     Serial.println(" Type in Box above, . ");
   Serial.println("(Decimal)(Hex)(Character)");  
@@ -37,8 +42,12 @@ digitalWrite(Clock_Pin, HIGH);
 //SPI.setBitOrder(MSBFRIST);
 //SPI.setClockDivider(SPI_CLOCK_DIV128);
 //SPI.setDataMode(SPI_MODE3);
-SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV128,MSBFIRST,SPI_MODE3));
+// for (16/2)/128 - enable the two CLKPR lines 
+//SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV128,MSBFIRST,SPI_MODE3));
+// for 16/32, resulting clock speed of 500 khZ
+SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV32,MSBFIRST,SPI_MODE3));
         
+
 InitLens();
 
 }
@@ -53,6 +62,27 @@ void InitLens()
   delay(50);
   SPI.transfer(0x00);
   delay(50);
+
+  //extra unblocks
+  //SPI.transfer(0x09);
+  //delay(50);
+  //SPI.transfer(0x00);
+  //delay(50);
+  //SPI.transfer(0x0E);
+  //delay(50);
+  //SPI.transfer(0x00);
+  //delay(50);
+  //SPI.transfer(0x0F);
+  //delay(50);
+  //SPI.transfer(0x00);
+  //delay(50);
+  //SPI.transfer(0x0A);
+  //delay(50);
+  //SPI.transfer(0x00);
+  //delay(50);
+
+  delay(50);
+
 }
 
 void loop() {     
@@ -73,6 +103,9 @@ if(ByteReceived == 'I') // upon shutter release
 {         
 
       InitLens();
+      // for stm?
+      //SPI.transfer(0x0C);
+      //delay(50);
       SPI.transfer(0x06);
       Serial.print(" FOCUS TO INFINITY");
       delay(50);
@@ -82,6 +115,9 @@ if(ByteReceived == 'Z') // upon shutter release
 {         
 
       InitLens();
+      // for stm?
+      //SPI.transfer(0x0C);
+      //delay(50);
       SPI.transfer(0x05);
       Serial.print(" FOCUS TO ZERO");
       delay(50);
@@ -96,7 +132,7 @@ InitLens();
       x = highByte(offset);
       y = lowByte(offset);
 
-      SPI.transfer(68);
+      SPI.transfer(0x44);
       delay(50);
       SPI.transfer(x);
       delay(50);
@@ -117,7 +153,7 @@ InitLens();
       x = highByte(offset);
       y = lowByte(offset);
 
-      SPI.transfer(68);
+      SPI.transfer(0x44);
       delay(50);
       SPI.transfer(x);
       delay(50);
@@ -125,6 +161,25 @@ InitLens();
       delay(50);     
       SPI.transfer(0);     
       Serial.print(" MOVE LENS -50");
+      delay(50);      
+
+}
+
+if(ByteReceived == 'A') // upon shutter release     
+{         
+
+InitLens();
+
+      SPI.transfer(0x07);
+      delay(50);
+      SPI.transfer(0x13);
+      delay(50);
+      SPI.transfer(0x05);
+      delay(50);     
+      SPI.transfer(0x08);     
+      delay(50);     
+      SPI.transfer(0x00);     
+      Serial.print(" APERTURE 5");
       delay(50);      
 
 }
